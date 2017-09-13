2f6ba4314abe14c9920cd7489b02ed32d2c29802
Yutaro Hayakawa <river@ht.sfc.keio.ac.jp>
2017-09-13 15:05:53 +0900

    Ported test frameworks for userspace from ubpf

---
+++ sys/dev/ebpf/test.c
@@ -0,0 +1,17 @@
+enum ebpf_ds_type { LIST };
+
+struct ebpf_ds_def {
+  enum ebpf_ds_type type;
+  unsigned long long key_size;
+  unsigned long long val_size;
+};
+
+#define SEC(sec_name) __attribute__((section(sec_name)))
+
+struct ebpf_ds_def SEC("ds") ds1 = {
+    .type = LIST, .key_size = 100, .val_size = 100};
+
+struct ebpf_ds_def SEC("ds") ds2 = {
+    .type = LIST, .key_size = 100, .val_size = 100};
+
+int test(int a) { return a; }
