700c005d0452f4d0fdb0cf8f37d8fb878c1343c7
Yutaro Hayakawa <river@ht.sfc.keio.ac.jp>
2018-01-01 10:27:56 +0100

    Fixed license text of Linux kernel codes

--- Linux/ebpf/kernel/ebpf_linux.h
+++ Linux/ebpf/kernel/ebpf_linux.h
@@ -1,21 +1,12 @@
-/*
- * Copyright 2017 Yutaro Hayakawa
+/* 
+ * Copyright (c) 2017 Yutaro Hayakawa
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of version 2 of the GNU General Public
+ * License as published by the Free Software Foundation.
  */
 
-#ifndef _EBPF_LINUX_H_
-#define _EBPF_LINUX_H_
+#pragma once
 
 #include <linux/kernel.h>
 #include <linux/module.h>
@@ -38,5 +29,3 @@
 #define htobe16(x) cpu_to_be16(x)
 #define htobe32(x) cpu_to_be32(x)
 #define htobe64(x) cpu_to_be64(x)
-
-#endif /* _EBPF_LINUX_H_ */
