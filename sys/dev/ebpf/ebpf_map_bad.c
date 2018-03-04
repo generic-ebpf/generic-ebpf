9847b7e4522bb234b3771f03b570a1cac641ffc5
Yutaro Hayakawa <river@ht.sfc.keio.ac.jp>
2018-03-04 22:41:56 +0100

    Added new map type "Bad" for testing and place holder of free ebpf_map_ops entries

---
+++ sys/dev/ebpf/ebpf_map_bad.c
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2017 Yutaro Hayakawa
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "ebpf_map.h"
+
+static int
+bad_map_init(struct ebpf_map *self, uint16_t key_size, uint16_t value_size,
+         uint16_t max_entries, uint32_t flags)
+{
+  return EINVAL;
+}
+
+static int
+bad_map_update_elem(struct ebpf_map *self, void *key, void *value,
+    uint64_t flags)
+{
+  return EINVAL;
+}
+
+static void *
+bad_map_lookup_elem(struct ebpf_map *self, void *key, uint64_t flags)
+{
+  return NULL;
+}
+
+static int
+bad_map_delete_elem(struct ebpf_map *self, void *key)
+{
+	return EINVAL;
+}
+
+static int
+bad_map_get_next_key(struct ebpf_map *self, void *key, void *next_key)
+{
+  return EINVAL;
+}
+
+static void
+bad_map_deinit(struct ebpf_map *self, void *arg)
+{
+  return;
+}
+
+struct ebpf_map_ops bad_map_ops = {
+  .init = bad_map_init,
+  .update_elem = bad_map_update_elem,
+  .lookup_elem = bad_map_lookup_elem,
+  .delete_elem = bad_map_delete_elem,
+  .get_next_key = bad_map_get_next_key,
+  .deinit = bad_map_deinit
+};
