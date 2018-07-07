093ca9f7876c77246d59ddfac98cf63a08488956
Yutaro Hayakawa <river@ht.sfc.keio.ac.jp>
2018-07-08 00:22:39 +0900

    Updated benchmark graph and plot script

--- benchmark/map_bench_plot.py
+++ benchmark/map_bench_plot.py
@@ -37,7 +37,7 @@ for i, t in enumerate(["hashtable"]):
             ax[mode].set_title("Benchmark type: %s-%s" %
                     (bench.title(), "Forward" if mode == 0 else "Random"))
             ax[mode].set_xticklabels(plot.get_xticklabels(), rotation=-40)
-            ax[mode].set(xlabel="Number of Objects [K]", ylabel="Time [usecs]")
+            ax[mode].set(xlabel="Number of Objects [K]", ylabel="Time [usecs]", ylim=(0, 120000))
 
         plt.subplots_adjust(
             left   = left,
